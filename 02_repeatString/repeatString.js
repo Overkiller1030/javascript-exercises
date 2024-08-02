4227d06a50dbb81d6cdf41e059e13fc2b7c4e542
Eric Rodgers <ejrodgers8694@gmail.com>
2024-08-01 22:52:35 -0500

    Complete javascript exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  completeString = '';
+  for(i = 0; i < num; i++) {
+    completeString += string;
+  };
+  if (num < 0){
+    return 'ERROR'
+  }
+  return completeString;
 };
 
 // Do not edit below this line
