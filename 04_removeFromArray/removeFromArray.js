a5145d6c3c58c2fb2a113d756f3869515129c798
Eric Rodgers <ejrodgers8694@gmail.com>
2024-08-02 23:44:22 -0500

    Complete javascript exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-};
-
+const removeFromArray = function(array, ...args) {
+  return array.filter(item => !args.includes(item));
+  // returns a filtered array by filtering original array items, but not including items from args.
+}
 // Do not edit below this line
 module.exports = removeFromArray;
